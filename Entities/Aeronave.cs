03ee8379b8a5372a7c6b8ae1a224d0714f1151d4
CleitsonLima <cleitson.ferreira@aluno.senai.br>
2022-08-17 08:40:00 -0300

    primeiro commit

---
+++ Entities/Aeronave.cs
@@ -0,0 +1,30 @@
+// Namespace é um conjunto de classes
+//Namespace é uma divisão lógica
+namespace VoeAirlinesSenai.Entities;
+
+// Classe é o conjunto de objetos
+// Objeto é uma instância de uma classe
+public class Aeronave{
+    public Aeronave(string fabricante, string codigo, string modelo)
+    {
+        Fabricante = fabricante;
+        Codigo = codigo;
+        Modelo = modelo;
+    }
+
+    public int Id{ get;set; }
+    public string Fabricante { get;set; }
+    public string Codigo { get;set; }
+    public string Modelo { get;set; }
+
+    public ICollection<Manutencao> Manutencoes { get; set; }
+
+
+}
+
+    // Properiedades Automáticas
+    // Características do objeto
+    // Automático: gera get e set
+    // Metodo set - atribui
+    // Método get - recupera
+    // POCO - o foco é o objeto
---
+++ Entities/Manutencao.cs
@@ -0,0 +1,27 @@
+namespace VoeAirlinesSenai.Entities;
+
+using VoeAirlinesSenai.Entities.Enums;
+
+public class Manutencao{
+    public Manutencao(DateTime dataHora, string observacoes, TipoManutencao tipo, int aeronaveId)
+    {
+        DataHora = dataHora;
+        Observacoes = observacoes;
+        this.tipo = tipo;
+        AeronaveId = aeronaveId;
+    }
+
+    public int Id { get;  set; }
+    public DateTime DataHora { get; set; }
+    public string Observacoes { get; set; }
+
+    public TipoManutencao tipo { get; set; }
+
+    public int AeronaveId { get; set; }
+
+    public Aeronave Aeronave { get; set; }=null!;
+
+    
+
+
+}
